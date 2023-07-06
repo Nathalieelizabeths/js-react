467cc79ce6bc7a8b9d494f1cf4b78e5815e451bb
Nathalieelizabeth <elizabethsnathalie@gmail.com>
2023-07-06 10:58:56 +0300

    commit

--- src/Products/index.js
+++ src/Products/index.js
@@ -25,8 +25,9 @@ const Products = ()=>{
         return <h2>Loading.....</h2>
     }
     return(
+        <>
+        <h1>All products</h1>
         <div className="product-grid">
-            <h1>All products</h1>
             {products.map(item => (
                 <div key={item.id} className="product-card">
                 <h2>{item.title}</h2>
@@ -38,6 +39,7 @@ const Products = ()=>{
                 </div>
             ))}
         </div>
+        </>
     );
 };
 export default Products;
