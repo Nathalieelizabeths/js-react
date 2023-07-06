6274a122ba275f33428710b440048d74e5d75b4b
Nathalieelizabeth <elizabethsnathalie@gmail.com>
2023-07-06 14:15:46 +0300

    commit

---
+++ src/Addproducts/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './style.css'
+// import { Link } from 'react-router-dom';
+// import Products from "../Products";
+const AddProducts = () => {
+    <div>
+        <input placeholder="EnterProductname" type="text" />
+        <button type="submit">Add product</button>
+    </div>
+}
+export default AddProducts
--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
+import AddProducts from './Addproducts';
 import Login from './Login';
+import Navigation from './Navigation';
 import Products from './Products';
+import Productdetails from './Productdetails';
+
 
 function App() {
   return (
     <div >
      <Login/>
      <Products/>
+     <Productdetails/>
+     <AddProducts/>
+     <Navigation/>
     </div>
   );
 }
